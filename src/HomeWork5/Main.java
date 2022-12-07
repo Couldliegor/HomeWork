be0b28e34232faa397cfcc8d5541ebc249325451
egorcouldlie <couldliegor@gmail.com>
2022-12-07 10:40:05 +0300

    exercise 1

--- src/HomeWork5/Main.java
+++ src/HomeWork5/Main.java
@@ -2,6 +2,13 @@ package HomeWork5;
 
 public class Main {
     public static void main(String[] args) {
-
+        task1();
+    }
+    public static void task1(){
+        //exercise 1
+        for(int i = 1; i <= 10; i++){
+            System.out.println(i);
+        }
+        //done
     }
 }
