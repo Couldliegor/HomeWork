1a4f014b379c9cca5e1f29f37bb264d6e14493e8
egorcouldlie <couldliegor@gmail.com>
2022-12-07 10:42:14 +0300

    exercise 2

--- src/HomeWork5/Main.java
+++ src/HomeWork5/Main.java
@@ -3,10 +3,19 @@ package HomeWork5;
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
     public static void task1(){
         //exercise 1
         for(int i = 1; i <= 10; i++){
+            System.out.println(i);
+        }
+        //done
+    }
+    public static void  task2(){
+        //exercise 2
+        System.out.println("\n");
+        for(int i = 10; i > 0; i--){
             System.out.println(i);
         }
         //done
